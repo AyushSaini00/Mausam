98a0e4c39024b07f15d1a756a2cb36a61ad59f16
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-03-28 17:15:26 +0530

    component: Search

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+const Search = () => {
+    return (
+        <div className="search">
+            <div className="search-box">
+                <label htmlFor="search-city">Enter City</label>
+                <input 
+                    type="text"
+                    id="search-city" 
+                />
+            </div>
+        </div>
+    );
+}
+ 
+export default Search;
