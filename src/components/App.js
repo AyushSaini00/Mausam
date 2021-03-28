351070557c6fa9fd5c88685a1ee13267aa23988a
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-03-28 14:52:02 +0530

    Component: Header

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header"
+
+const App = () => {
+    return ( 
+        <div className="App">
+            <Header />
+            <div className="container">
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+const Header = () => {
+    return ( 
+        <header className="header">
+            <div className="logo">
+                ☁ Mausam
+            </div>
+            <div className="links">
+                <button className="btn">
+                    <a href="/about">About</a>
+                </button>
+                <button className="btn btn-github">
+                    <span className="github-star">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" focusable="false">
+                            <path d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"></path>
+                        </svg>
+                    </span>
+                    &nbsp;
+                    <a 
+                        href="https://github.com/AyushSaini00/Mausam" 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >Github</a>
+                </button>
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
